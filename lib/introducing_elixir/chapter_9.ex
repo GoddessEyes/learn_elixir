85346c79e4cf4f1eca4fab090bd027566d7cb554
goddesseyes <goddesseyes@protonmail.com>
2023-01-19 02:09:42 +0300

    chapter 9

--- lib/introducing_elixir/chapter_9.ex
+++ lib/introducing_elixir/chapter_9.ex
@@ -1,30 +1,55 @@
 # Chapter 9. Process
 
-pid = self()
-
-send(pid, :test)
-
-# flush() for iex
-send(pid, :test)
-
-receive do
-  value -> value
-end
-
-
-defmodule Bounce do
-  def report do
-    receive do
-      val ->
-        IO.puts("Received #{val} message")
-        report()
-    end
-  end
-end
-
 # For iex test:
+
+# pid = self()
+
+# send(pid, :test)
+
+# # flush() for iex
+# send(pid, :test)
+
+# receive do
+#   value -> value
+# end
+
+
+# defmodule Bounce do
+#   def report do
+#     receive do
+#       val ->
+#         IO.puts("Received #{val} message")
+#         report()
+#     end
+#   end
+# end
+
 # pid = spawn(Bounce, :report, [])
 # send(pid, :test)
 
-Process.register(self(), :proc_name)
-send(:proc_name, :test)
+# Process.register(self(), :proc_name)
+# send(:proc_name, :test)
+
+
+defmodule IntroducingElixir.Chapter9.Drop do
+  def drop do
+    receive do
+      {from, planemo, distance} ->
+        send(from, {planemo, distance, fall_velocity({planemo, distance})})
+        drop()
+    end
+
+  end
+
+  defp fall_velocity({:earth, distance}) when distance >= 0 do
+    :math.sqrt(2 * 9.8 * distance)
+  end
+
+  defp fall_velocity({:moon, distance}) when distance >= 0 do
+    :math.sqrt(2 * 1.6 * distance)
+  end
+
+  defp fall_velocity({:mars, distance}) when distance >= 0 do
+    :math.sqrt(2 * 3.71 * distance)
+  end
+end
