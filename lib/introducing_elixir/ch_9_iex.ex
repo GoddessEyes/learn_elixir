493e6144b916d2101eb8903a01ef1314a25c794b
goddesseyes <goddesseyes@protonmail.com>
2023-01-19 23:57:32 +0300

    fix ch_9_iex

---
+++ lib/introducing_elixir/ch_9_iex.ex
@@ -0,0 +1,33 @@
+# Chapter 9: Process
+
+
+pid = self()
+
+send(pid, :test)
+
+# flush() for iex
+send(pid, :test)
+
+receive do
+  value -> value
+end
+
+
+defmodule Bounce do
+  def report do
+    receive do
+      val ->
+        IO.puts("Received #{val} message")
+        report()
+    end
+  end
+end
+
+# Or use spawn_link/3
+pid = spawn(Bounce, :report, [])
+send(pid, :test)
+
+
+
+Process.register(self(), :proc_name)
+send(:proc_name, :test)
