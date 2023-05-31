65fea6dfbd4ddded7b2f5245431c895135fac8ba
goddesseyes <goddesseyes@protonmail.com>
2023-05-31 20:21:10 +0300

    Add ch6 GenServer

---
+++ lib/elixir_in_action/ch6.ex
@@ -0,0 +1,52 @@
+
+defmodule KeyValueStore do
+  @moduledoc """
+    ## Examples
+      iex> {:ok, pid} = KeyValueStore.start()
+  		{:ok, #PID<0.574.0>}
+      iex> KeyValueStore.put(pid, :some_key, :some_value)
+      :ok
+      iex> KeyValueStore.put(pid, :some_key1, :some_value)
+      :ok
+      iex> KeyValueStore.all(pid)
+      %{some_key: :some_value, some_key1: :some_value}
+      iex> KeyValueStore.get(pid, :some_key)
+      :some_value
+  """
+
+  use GenServer
+
+  # Interface func:
+  def start do
+    GenServer.start(KeyValueStore, nil)
+  end
+
+  def put(pid, key, value) do
+    GenServer.cast(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
+  def all(pid) do
+    GenServer.call(pid, :all)
+  end
+
+  # Genserver impl:
+  def init(_) do
+    {:ok, %{}}
+  end
+
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, _, state) do
+    {:reply, Map.get(state, key), state}
+  end
+
+  def handle_call(:all, _, state) do
+    {:reply, state, state}
+  end
+end
