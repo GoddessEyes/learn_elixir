649dc94d0f780c41343ce7e5670b4229f109dea4
goddesseyes <goddesseyes@protonmail.com>
2023-06-01 18:51:17 +0300

    Add ch6 TodoList with GenServer realization

--- lib/elixir_in_action/ch6.ex
+++ lib/elixir_in_action/ch6.ex
@@ -1,4 +1,4 @@
-
+## KVStore server realization:
 defmodule KeyValueStore do
   @moduledoc """
     ## Examples
@@ -6,7 +6,7 @@ defmodule KeyValueStore do
   		{:ok, #PID<0.574.0>}
       iex> KeyValueStore.put(pid, :some_key, :some_value)
       :ok
-      iex> KeyValueStore.put(pid, :some_key1, :some_value)
+      iex> KeyValueStore.put(:some_key1, :some_value)
       :ok
       iex> KeyValueStore.all(pid)
       %{some_key: :some_value, some_key1: :some_value}
@@ -18,11 +18,11 @@ defmodule KeyValueStore do
 
   # Interface func:
   def start do
-    GenServer.start(KeyValueStore, nil)
+    GenServer.start(__MODULE__, nil, name: KeyValueStore)
   end
 
-  def put(pid, key, value) do
-    GenServer.cast(pid, {:put, key, value})
+  def put(key, value) do
+    GenServer.cast(__MODULE__, {:put, key, value})
   end
 
   def get(pid, key) do
@@ -34,19 +34,106 @@ defmodule KeyValueStore do
   end
 
   # Genserver impl:
+
+  @impl GenServer
   def init(_) do
     {:ok, %{}}
   end
 
+  @impl GenServer
   def handle_cast({:put, key, value}, state) do
     {:noreply, Map.put(state, key, value)}
   end
 
+  @impl GenServer
   def handle_call({:get, key}, _, state) do
     {:reply, Map.get(state, key), state}
   end
 
+  @impl GenServer
   def handle_call(:all, _, state) do
     {:reply, state, state}
+  end
+end
+
+
+## TodoList realization:
+defmodule TodoServer do
+  use GenServer
+
+  def start do
+    GenServer.start(TodoServer, nil)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+  @impl GenServer
+  def init(_) do
+    {:ok, TodoList.new()}
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, new_entry}, todo_list) do
+    new_state = TodoList.add_entry(todo_list, new_entry)
+    {:noreply, new_state}
+  end
+
+  @impl GenServer
+  def handle_call({:entries, date}, _, todo_list) do
+    {
+      :reply,
+      TodoList.entries(todo_list, date),
+      todo_list
+    }
+  end
+end
+
+defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
+
+  def new(entries \\ []) do
+    Enum.reduce(
+      entries,
+      %TodoList{},
+      &add_entry(&2, &1)
+    )
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    %TodoList{todo_list | entries: Map.delete(todo_list.entries, entry_id)}
   end
 end
